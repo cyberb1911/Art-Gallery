6c0608dfcbca68d9155d869268c9427cf83268c3
cyberb1911 <cyberb1911@gmail.com>
2019-04-24 17:50:08 -0400

    PaintingTableViewCell and PaintController install Assign these to the controller and table view in Interface Builder using the identity inspector

---
+++ Art-Gallery/Art-Gallery/PaintingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaintingTableViewCell.swift
+//  Art-Gallery
+//
+//  Created by Brian Cobb on 4/24/19.
+//  Copyright © 2019 Kappa. All rights reserved.
+//
+
+import UIKit
+
+class PaintingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
